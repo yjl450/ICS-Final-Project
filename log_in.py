888d22378a53c694cbc2f321262cc193300d1d81
yjl450 <32358997+yjl450@users.noreply.github.com>
2018-12-05 21:22:10 +0800

    Update log_in.py

--- log_in.py
+++ log_in.py
@@ -102,21 +102,27 @@ class Ui_log_in(object):
                                          QMessageBox.Ok, QMessageBox.Ok)
 
     def reg_bnt_click(self):
-        path, type = QFileDialog.getOpenFileName(None,
-                                                 "Register your photo",
-                                                 "",
-                                                 "All Files (*);;Image File (*.jpg)")
         name = self.newUserName.text()
-        reg_result = chat_face_recog.reg(path, name)
-        if reg_result == True:
-            reply = QMessageBox.question(None, 'Success', 'User' + name + ' is successfully registered!\n You can login now!',
+        if name == '':
+            reply = QMessageBox.question(None, 'No Username','Please enter a username!',
                                          QMessageBox.Ok, QMessageBox.Ok)
-        elif reg_result == 'User already exists':
-            reply = QMessageBox.question(None, 'Existing User', 'User ' + name + ' already exists!',
-                                         QMessageBox.Ok, QMessageBox.Ok)
-        elif reg_result == 'No face Detected':
-            reply = QMessageBox.question(None, 'Invalid Photo', 'No face detected in the photo!\nPlease select another photo!',
-                                         QMessageBox.Ok, QMessageBox.Ok)
+        else:
+            path, type = QFileDialog.getOpenFileName(None,
+                                                     "Register your photo",
+                                                     "",
+                                                     "All Files (*);;Image File (*.jpg)")
+            reg_result = chat_face_recog.reg(path, name)
+            if reg_result == True:
+                reply = QMessageBox.question(None, 'Success',
+                                             'User ' + name + ' is successfully registered!\n You can login now!',
+                                             QMessageBox.Ok, QMessageBox.Ok)
+            elif reg_result == 'User already exists':
+                reply = QMessageBox.question(None, 'Existing User', 'User ' + name + ' already exists!',
+                                             QMessageBox.Ok, QMessageBox.Ok)
+            elif reg_result == 'No face Detected':
+                reply = QMessageBox.question(None, 'Invalid Photo',
+                                             'No face detected in the photo!\nPlease select another photo!',
+                                             QMessageBox.Ok, QMessageBox.Ok)
 
 
 def main():
